dbadbc4934a192764b236f808a9016f8141e90ab
jlzhang <2506988720@qq.com>
2020-04-29 14:32:34 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,9 +14,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/api/hello")
-    public String hello(){
-        return "hello jenkins";
+    @GetMapping("/api/hello/{name}")
+    public String hello(@PathVariable(name = "name")String name){
+        return "hello"+name;
     }
 
 }
