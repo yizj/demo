dc157c7beee750ee8f3629b8b3d39ab4bb7312c4
lzz <2506988720@qq.com>
2020-05-08 20:05:58 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +15,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/api/hello/{name}")
-    public String hello(@PathVariable(name = "name")String name){
-        return "hello"+name;
+    @GetMapping("/api/hello")
+    public String hello(@RequestParam(name = "name")String name,@RequestParam(name = "age")String age){
+        return "hello"+name+age;
     }
 
 }
