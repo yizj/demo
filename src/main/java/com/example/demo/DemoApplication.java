fef9d4a8b4d7b350b59a163f3bf11116ffaa909b
lzz <2506988720@qq.com>
2020-06-02 20:24:35 +0800

    frature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -13,15 +18,11 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/api/hello")
-    public String hello(){
-        return "hello jenkins";
+    @GetMapping("/api/strings")
+    public List<String> hello(){
+        List<String> strings= Arrays.asList("11","",null);
+        return strings.stream().filter(s-> !StringUtils.isEmpty(s)).collect(Collectors.toList());
     }
 
-
-    @GetMapping("/api/user")
-    public String user(){
-        return "hello user";
-    }
 
 }
